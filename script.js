909037c2fbd1766596380a3cf90149d1633b4637
Luis Castillo <mr.luiscastillo@gmail.com>
2022-02-23 23:29:27 -0500

    initialize fn for nextBtn

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const nextBtn = document.querySelector(".next-btn");
 const prevBtn = document.querySelector(".prev-btn");
 const slides = document.querySelectorAll(".slide");
 const slideIcons = document.querySelectorAll(".slide-icon")
+const numberOfSlides = slides.length;
+let slideNumber = 0;
+
+nextBtn.addEventListener("click", () => {
+    slideNumber++;
+
+    if(slideNumber > (numberOfSlides - 1)){
+        slideNumber = 0;
+    }
+
+    slides[slideNumber].classList.add("active")
+    slideIcons[slideNumber].classList.add("active")
+});
