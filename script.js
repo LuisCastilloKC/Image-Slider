53af039253042732808a470cb4e8324b331ff0f7
Luis Castillo <mr.luiscastillo@gmail.com>
2022-02-23 23:27:25 -0500

    select btn, slide, slide-icon and add script serch to index

---
+++ script.js
@@ -0,0 +1,4 @@
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const slides = document.querySelectorAll(".slide");
+const slideIcons = document.querySelectorAll(".slide-icon")
